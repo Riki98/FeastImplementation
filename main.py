6d4422ca446f47ec719733c9b520176a33557078
Riki98 <riccardocoro98@gmail.com>
2021-11-29 12:14:08 +0100

    Prove per proiezione grafo

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import datetime
+from pprint import pprint
+from feast import FeatureStore
+
+store = FeatureStore(repo_path=".")
+
+feature_vector = store.get_online_features(
+    features=[
+        "driver_hourly_stats:conv_rate",
+        "driver_hourly_stats:acc_rate",
+        "driver_hourly_stats:avg_daily_trips",
+    ],
+    entity_rows=[
+        {"driver_id": 1004},
+        {"driver_id": 1005},
+    ],
+).to_dict()
+
+pprint(feature_vector)
+
+pd.read_parquet("feast_repo/data/driver_stats.parquet") #anche con la demo è vuoto, vedere come riempire
+print("forte")
