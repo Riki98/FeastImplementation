0a1c031a5519a3050ee0d135ed5fce58d0c20824
Riki98 <riccardocoro98@gmail.com>
2022-01-03 14:03:58 +0100

    Datasource funzionante

--- feature_repo/neo4j_datasource.py
+++ feature_repo/neo4j_datasource.py
@@ -26,21 +26,12 @@ con = get_sqlalchemy_engine(offline_config)
 
 ############################################################ FUNZIONI NEO4J ##################################################################
 
+# funziona
 def run_retrieve_neo4j_table(table_name : String, column_name : list[String] = None) :
-    table_query = "MATCH (t:Author) RETURN t.name"
+    table_query = f"MATCH (t:{table_name}) RETURN "
     df_res = None
     if column_name == None : 
         table_query += "t LIMIT 2"
-            
-    else :
-        str_to_append = f"""t.{column_name[0]} """
-        table_query += str_to_append
-        column_name.pop(0)
-        for column in column_name :
-            str_to_append = f""", t.{column} """
-            table_query += str_to_append
-        res_query = driver.run_transaction_query(table_query, run_query=driver.run_query_return_data_key)
-        df_res = pd.DataFrame.from_dict(res_query)
     res_query = driver.run_transaction_query(table_query, run_query=driver.run_query_return_data)
     print(res_query)
     res_query = [x["t"] for x in res_query["data"]]
@@ -48,29 +39,27 @@ def run_retrieve_neo4j_table(table_name : String, column_name : list[String] = N
     return df_res
 
 
-
-def run_store_data(table_name : String, df_table : pd.DataFrame, if_exist : String) : 
-    var_temp = pd.Timestamp.now()
-    df_table["event_timestamp"] = var_temp
-    df_table["created"] = var_temp
-    if if_exist == "replace" : 
-        df_table.to_sql(table_name, con, offline_config.db_schema, if_exists="replace", index=False) # rimpiazzo tutta la tabella su postgres
-    elif if_exist == "append" : 
-        df_table.to_sql(table_name, con, offline_config.db_schema, if_exists="append", index=False) # aggiungo i dati alla tabella su postgres
-
-
-
+# funziona
 def run_create_offline_table(table_name : String, df_table : pd.DataFrame, if_exist : String = None):
     var_temp = pd.Timestamp.now()
     df_table["event_timestamp"] = var_temp
     df_table["created"] = var_temp
     if if_exist == "replace" : 
-        df_table.to_sql(table_name, con, offline_config.db_schema, if_exists="replace", index=False) # rimpiazzo tutta la tabella su postgres
+        df_table.to_sql(table_name, con, offline_config.db_schema, if_exists="replace") # rimpiazzo tutta la tabella su postgres
+    else :
+        df_table.to_sql(table_name, con, offline_config.db_schema) # creo la tabella su postgres
+
+#funziona, anche se maiuscole e minuscole nel nome della tabella non fanno differenza
+def run_drop_offline_table(table_name : String) :
+    print(table_name)
+    con.execute(f"DROP TABLE IF EXISTS {table_name}")
+
+# se append non è specificato, creo una nuova tabello su cui fare lo store dei dati
+def run_store_data(table_name : String, df_table : pd.DataFrame, if_exist : String = None) : 
+    var_temp = pd.Timestamp.now()
+    df_table["event_timestamp"] = var_temp
+    df_table["created"] = var_temp
+    if if_exist == "append" : 
+        df_table.to_sql(table_name, con, offline_config.db_schema, if_exists="append") # aggiungo i dati alla tabella su postgres
     else : 
-        df_table.to_sql(table_name, con, offline_config.db_schema, index=False) # creo la tabella su postgres
-
-
-def run_delete_offline_table(table_name : String) :
-    con.execute(f"DROP CASCADE TABLE IF EXISTS {table_name}")
-
-
+        df_table.to_sql(table_name, con, offline_config.db_schema) # creo una tabella su postgres
